7e570b16ed286c714c66607b9bf0220cdc253294
J Cheyo Jimenez <cheyo@masters3d.com>
2017-06-19 15:11:39 -0700

    added constructor

--- js/app.js
+++ js/app.js
@@ -1,3 +1,53 @@
 'use strict';
 
 console.log('hello world!');
+
+function Photo(fileName, fileType) {
+  this.fileName = fileName;
+  this.fileType = fileType;
+}
+
+var photos = [];
+
+// Looks to see if the object is in the stores array
+stores.containsPhoto = function(photo) {
+  for (var eaIndex in this ) {
+    if (this[eaIndex].fileName === photo.fileName) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+// Resets all the photos to the ones on file
+photos.setPhotoObjects = function (){
+  photos = [];
+  var photosInfo = [
+  ['bag', 		  'jpg'],
+  ['banana', 		'jpg'],
+  ['bathroom', 	'jpg'],
+  ['boots', 		'jpg'],
+  ['breakfast',	'jpg'],
+  ['bubblegum', 'jpg'],
+  ['chair', 		'jpg'],
+  ['cthulhu', 	'jpg'],
+  ['dog-duck', 	'jpg'],
+  ['dragon', 		'jpg'],
+  ['pen', 		  'jpg'],
+  ['pet-sweep', 'jpg'],
+  ['scissors', 	'jpg'],
+  ['shark', 		'jpg'],
+  ['sweep', 		'png'],
+  ['tauntaun', 	'jpg'],
+  ['unicorn', 	'jpg'],
+  ['usb', 		  'gif'],
+  ['water-can', 'jpg'],
+  ['wine-glass','jpg']
+  ];
+  for(var ea in photosInfo){
+    photos.push(
+    new Photo( photosInfo[ea][0], photosInfo[ea][1] )
+  );
+  }
+};
